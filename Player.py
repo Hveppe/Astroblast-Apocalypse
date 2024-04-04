276df4664fdf94132061a42e32e369ae369e5fba
Hveppe <mikk018t@edu.aarhustech.dk>
2024-04-04 20:23:31 +0200

    Gjorde det muligt for PlayerClass at bevæge sig

--- Player.py
+++ Player.py
@@ -5,11 +5,18 @@ class PlayerClass:
     colour = (255, 255, 255)
     width = 20
     height = 20
+    xmove = 0
+    ymove = 0
+    movespeed = 1
 
     def __init__(self, screen, xvalue, yvalue):
         self.screen = screen
         self.x = xvalue
         self.y = yvalue
+
+    def update(self):
+        self.x += self.xmove
+        self.y += self.ymove
 
     def draw(self):
         pygame.draw.rect(self.screen, self.colour, pygame.Rect(self.x, self.y, self.width, self.height))
--- main.py
+++ main.py
@@ -20,8 +20,33 @@ while gamerunning:
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             gamerunning = False
 
+        # controls til PlayerClass
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                player.ymove -= player.movespeed
+            if event.key == pygame.K_s:
+                player.ymove += player.movespeed
+            if event.key == pygame.K_d:
+                player.xmove += player.movespeed
+            if event.key == pygame.K_a:
+                player.xmove -= player.movespeed
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:
+                player.ymove += player.movespeed
+            if event.key == pygame.K_s:
+                player.ymove -= player.movespeed
+            if event.key == pygame.K_d:
+                player.xmove -= player.movespeed
+            if event.key == pygame.K_a:
+                player.xmove += player.movespeed
+
+    # Farver baggrund
     display.fill((0, 0, 0))
 
+    # Tegner player
+    player.update()
     player.draw()
 
+    # Updater display
     pygame.display.flip()
