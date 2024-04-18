419ebc4fe7be4833a726ce3a975d704f10c13dcf
Hveppe <mikk018t@edu.aarhustech.dk>
2024-04-18 09:38:06 +0200

    Tilføjede en button class

--- objekts.py
+++ objekts.py
@@ -3,6 +3,36 @@
 import pygame
 
 
+class Button:
+    def __init__(self, screen, x, y, image):
+        self.image = image
+        self.screen = screen
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.clicked = False
+        self.action = True
+
+    def draw(self):
+        self.action = False
+
+        # position af musen
+        mus_position = pygame.mouse.get_pos()
+
+        # check om musen er over knappen og om der trykkes på den
+        if self.rect.collidepoint(mus_position):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked is False:
+                self.clicked = True
+                self.action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        # tegn knappen
+        self.screen.blit(self.image, self.rect.topleft)
+
+        return self.action
+
+
 class Picture:
     def __init__(self, screen, x, y, image):
         self.image = image
