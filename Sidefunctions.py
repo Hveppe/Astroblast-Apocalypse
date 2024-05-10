1f942b4d3885beb9dd57c86a4a6d3a61a162c569
Hveppe <mikk018t@edu.aarhustech.dk>
2024-05-10 20:00:12 +0200

    Satte (x, y = destination) ind under center is True

--- Sidefunctions.py
+++ Sidefunctions.py
@@ -5,9 +5,9 @@ import pygame
 
 def tekst_render(font, tekst, destination, screen, color, center):
     tekst = font.render(tekst, True, color)
-    x, y = destination
 
     if center is True:
+        x, y = destination
         screen.blit(tekst, (x-tekst.get_width()/2, y))
     elif center is False:
         screen.blit(tekst, destination)
