fc4349b05aff8579ad8311f77c7ce53aa02f0ea9
Hveppe <mikk018t@edu.aarhustech.dk>
2024-05-10 20:38:51 +0200

    Lavede en fill til at define variabler og andre ting

---
+++ Define.py
@@ -0,0 +1,61 @@
+# Lavet af Hveppe
+
+import time
+
+# ---------------------------------------------Farver-------------------------------------------------------------------
+White = (255, 255, 255)
+Black = (0, 0, 0)
+Red = (255, 0, 0)
+Green = (0, 255, 0)
+Yellow = (255, 255, 0)
+Blue = (0, 0, 255)
+Orange = (255, 165, 0)
+
+# -------------------------------------------Variabler------------------------------------------------------------------
+# Antal fjender
+antalfjender = 2
+antalfjenderheavy = 0
+antalfjendermineswpper = 0
+antalfjenderhomming = 0
+debug = False
+
+# wave variabler
+wave = 1
+waveheavyspawn = 5
+wavemineswepperspawn = 6
+wavehommingspawn = 7
+new_wave_delay = 0.1
+new_wave_begin = None
+
+# andre ting
+lives = 5
+wavelives = 4
+minepoint = 0
+fjende_spawn = False
+taking_damage_player = False
+time_of_damage = time.time()
+time_being_red = 0.25
+explosion_time = 0.1
+
+# wave spawn ændringer
+waveheavyspawnadd = 5
+wavemineswepperspawnadd = 4
+wavehommingspawnadd = 4
+changeinrate = 10
+
+# laser
+delaylaser = 0.3
+last_time_shot = 0
+shotting = False
+lastmove = 'w'
+laser_color = Blue
+
+# shield varaibler
+shield_up = False
+shield_charge = 100
+drain_speed = 0.5
+last_draintime = None
+
+# giving time of start
+startgametime = time.time()
+timer = time.time()-startgametime
