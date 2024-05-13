76c3bc65f349adc78f8f1c3a017681bf38b017b9
Hveppe <mikk018t@edu.aarhustech.dk>
2024-05-13 11:00:24 +0200

    tilføjede kommentar

--- Healthbar.py
+++ Healthbar.py
@@ -18,7 +18,10 @@ class HealthBar:
         self.max_hp = max_hp
 
     def draw(self, surface):
+        # udrenger hvor mange procent liv man har
         ratio = self.hp / self.max_hp
+
+        # tegner healthbaren
         pygame.draw.rect(surface, self.red, (self.x, self.y, self.width, self.height))
         pygame.draw.rect(surface, self.green, (self.x, self.y, self.width * ratio, self.height))
         pygame.draw.rect(surface, self.white, (self.x, self.y, self.width, self.height), 2)
